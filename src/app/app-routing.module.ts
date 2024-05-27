76428150674667b02790528c2f0ed4a8e09e3dd8
josearcos2023 <jose.arcos@tecsup.edu.pe>
2024-05-27 18:19:16 -0500

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { Pagina1Component } from './components/pagina1/pagina1.component';
+import { Pagina2Component } from './components/pagina2/pagina2.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch:'full'},
+  {path: 'home', component:HomeComponent},
+  {path: 'pagina1', component:Pagina1Component},
+  {path: 'pagina2', component:Pagina2Component}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { Pagina1Component } from './components/pagina1/pagina1.component';
+import { Pagina2Component } from './components/pagina2/pagina2.component';
+import { NavmenuComponent } from './components/navmenu/navmenu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    Pagina1Component,
+    Pagina2Component,
+    NavmenuComponent
   ],
   imports: [
     BrowserModule,
